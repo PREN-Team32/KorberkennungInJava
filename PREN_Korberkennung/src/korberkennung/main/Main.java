2677453e121ac1236cef348b1a65cce92274b1d7
hizonomi <niklausmanser@hotmail.com>
2015-03-19 10:36:27 +0100

    corrected package names

--- PREN_Korberkennung/src/korberkennung/main/Main.java
+++ PREN_Korberkennung/src/korberkennung/main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package korberkennung.core;
+package korberkennung.main;
 
 import korberkennung.detektor.Detector;
 import korberkennung.desktopviewer.Viewer;
